b3e7cb3bd34800ce90f747df524911446ccdf3f5
Artsiom Balakir <abalak8@illinois.edu>
2019-04-14 20:22:40 +0000

    Alan, Zack: init data retrieval from the network

---
+++ visualization/nodeClass.py
@@ -0,0 +1,24 @@
+class nodeClass:
+    def __init__(self, public_key, tvu, tpu, storage_addr, rpc, latitude, longitude, city, region, country):
+        self.public_key = public_key
+        self.tvu = tvu
+        self.tpu = tpu
+        self.storage_addr = storage_addr
+        self.rpc = rpc
+        self.latitude = latitude
+        self.longitude = longitude
+        self.city = city
+        self.region = region
+        self.country = country
+
+    def printNodeInfo(self):
+        print("Public Key: ", self.public_key)
+        print("TVU: ", self.tvu)
+        print("TPU: ", self.tpu)
+        print("Storage Address: ", self.storage_addr)
+        print("RPC: ", self.rpc)
+        print("Latitude: ", self.latitude)
+        print("Longitude: ", self.longitude)
+        print("City: ", self.city)
+        print("Region: ", self.region)
+        print("Country: ", self.country)
