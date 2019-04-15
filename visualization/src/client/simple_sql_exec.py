c552f32a7733eafa9186fe29fc89ec8e22ad7d3c
Artsiom Balakir <abalak8@illinois.edu>
2019-04-15 02:43:50 +0000

    simple example of sql request from python

---
+++ visualization/src/client/simple_sql_exec.py
@@ -0,0 +1,14 @@
+import pymysql
+import pprint
+
+if __name__ == "__main__":
+    connection = pymysql.connect(
+        host='localhost', user="UIUC", password="UIUC_492")
+
+    with connection.cursor() as cursor:
+        sql_req = "show databases;"
+        cursor.execute(sql_req)
+        pprint.pprint(cursor.fetchall())
+
+    connection.close()
+
