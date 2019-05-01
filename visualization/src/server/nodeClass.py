f53a06007daca7a0e8e916c886dca39618123e68
Artsiom Balakir <abalak8@illinois.edu>
2019-05-01 17:05:13 +0000

    Implemented ping

---
+++ visualization/src/server/nodeClass.py
@@ -0,0 +1,29 @@
+class nodeClass:
+    def __init__(self, public_key, tvu, tpu, storage_addr, rpc, latitude=None, longitude=None, city=None, region=None, country=None, ping=None):
+        self.public_key = public_key
+        self.tvu = tvu
+        self.tpu = tpu
+        self.storage_addr = storage_addr
+        self.rpc = rpc
+        self.latitude = latitude
+        self.longitude = longitude
+        self.city = city
+        self.region = region
+        self.country = country
+        self.ping = ping
+
+    def printNodeInfo(self):
+        print("Public Key: ", self.public_key)
+        print("TVU: ", self.tvu)
+        print("TPU: ", self.tpu)
+        print("Storage Address: ", self.storage_addr)
+        print("RPC: ", self.rpc)
+        print("Latitude: ", self.latitude)
+        print("Longitude: ", self.longitude)
+        print("City: ", self.city)
+        print("Region: ", self.region)
+        print("Country: ", self.country)
+        print("Ping: ", self.ping)
+
+    def get_ip_address(self):
+        return self.tvu.split(":")[0]
---
+++ visualization/src/server/retrieve_info.py
@@ -0,0 +1,156 @@
+import getopt
+import ipapi
+import nodeClass
+import subprocess
+import sys
+import time
+import os
+from pythonping import ping
+
+def explore_network(printInfo=False):
+    output = subprocess.check_output("../../../target/debug/visualizer_rust --timeout 2 --network $(dig +short edge.testnet.solana.com):8001", shell=True)
+
+    output = output.decode("utf-8")
+    outputArray = output.splitlines()
+
+    nodes = []
+    numberOfNodes = int(outputArray[0])
+
+    for node_data_text in outputArray[1:]:
+        nodeInfo = [x.strip() for x in node_data_text.split(',')]
+        currentNode = nodeClass.nodeClass(nodeInfo[0], nodeInfo[1], nodeInfo[2], nodeInfo[3], nodeInfo[4])
+        nodes.append(currentNode)
+
+        if printInfo:
+            currentNode.printNodeInfo()
+
+    return nodes
+
+def get_network_info(nodes):
+    get_location_info(nodes)
+    ping_network(nodes)
+
+def get_location_info(nodes):
+    for node in nodes:
+        nodeIP = node.get_ip_address()
+        
+        node.latitude  = ipapi.location(nodeIP, None, 'latitude')
+        node.longitude = ipapi.location(nodeIP, None, 'longitude')
+        node.city = ipapi.location(nodeIP, None, 'city')
+        node.region = ipapi.location(nodeIP, None, 'region')
+        node.country = ipapi.location(nodeIP, None, 'country')
+
+
+def ping_network(nodes):
+    for node in nodes:
+        ip = node.get_ip_address()
+        start = time.time()
+        response = os.system("ping -c 1 " + ip)
+        pingTime = time.time() - start
+
+        if response == 0:
+            node.ping = pingTime * 100
+        else:
+            node.ping = -1
+    
+        #responseList = ping(ip)
+        #node.ping = responseList.rtt_avg_ms
+
+def upload_to_database(nodes):
+    pass
+
+def insert_good_name_here(iterations, log_data, sleep_time):
+    run_count = 0
+    while True:
+        nodes = explore_network()
+
+        get_network_info(nodes)
+
+        if log_data:
+            for node in nodes:
+                node.printNodeInfo()
+                print("")
+
+        upload_to_database(nodes)
+
+        run_count += 1
+        if iterations > 0 and run_count >= iterations:
+            print(iterations, run_count)
+            break
+        time.sleep(sleep_time)
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+
+    # Code from tutorialspoint, python command line arguments page
+    try:
+        opts, args = getopt.getopt(argv, "hi:lt:", ["help", "iterations=", "log", "time="])
+    except getopt.GetoptError:
+        print("Invalid arguments. Type --help or -h?")
+        sys.exit(2)
+
+    iterations = 0
+    log_data = True
+    sleep_time = 30
+
+    for opt, arg in opts:
+        if opt in ("--help", "-h"):
+            print("retrieve_info.py -i <iterations> -l -time <time>")
+            print("-i   Iterations is how many times the data is collected. 0 (default) is forever.")
+            print("-l   Log data.")
+            print("-t   Time between data collection. Default 10 seconds")
+        elif opt in ("-i", "--iterations"):
+            iterations = int(arg)
+            if iterations < 0:
+                print("Iterations must be a positive integer or 0")
+                sys.exit(2)
+        elif opt in ("-l", "--log"):
+            log_data = True
+        elif opt in ("-t", "--time"):
+            sleep_time = int(arg)
+            if sleep_time <= 0:
+                print("time must be a non-negative integer")
+                sys.exit(2)
+
+    insert_good_name_here(iterations, log_data, sleep_time)
+
+'''
+# gossip_process = subprocess.Popen(["../target/debug/solana-gossip", "--timeout", "5", "--network", "$(dig +short edge.testnet.solana.com):8001"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+# output = gossip_process.communicate()
+
+# output = subprocess.check_output(["../target/debug/solana-gossip", "--timeout", "5", "--network", "$(dig +short edge.testnet.solana.com):8001"], shell=True)
+
+output = subprocess.check_output("../target/debug/visualizer_rust --timeout 2 --network $(dig +short edge.testnet.solana.com):8001", shell=True)
+
+
+print("\n================\n")
+
+#output = "2\npk1,ss1,tvu1,tpu1,sa1,rpc1\npk2,ss2,tvu2,tpu2,sa2,rpc2\n"
+output = output.decode('utf-8')
+outputArray = output.splitlines()
+#outputArray = output
+
+#print(output)
+
+nodes = []
+numberOfNodes = int(outputArray[0])
+
+for i in range(numberOfNodes):
+    nodeInfo = [x.strip() for x in outputArray[i+1].split(',')]
+
+    nodeIP = nodeInfo[1].split(':')[0]
+    
+    lati = ipapi.location(nodeIP, None, 'latitude')
+    longi = ipapi.location(nodeIP, None, 'longitude')
+    city = ipapi.location(nodeIP, None, 'city')
+    region = ipapi.location(nodeIP, None, 'region')
+    country = ipapi.location(nodeIP, None, 'country')
+
+    currentNode = nodeClass.nodeClass(nodeInfo[0], nodeInfo[1], nodeInfo[2], nodeInfo[3], nodeInfo[4], lati, longi, city, region, country)
+    nodes.append(currentNode)
+
+for i in range(numberOfNodes):
+    nodes[i].printNodeInfo()
+    print("\n")
+
+'''
